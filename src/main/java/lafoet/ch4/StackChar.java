f804ce8e5bbc055e15ac47204f4738c845a9db9c
Aleksey Sorokin <asorokin@megar.ru>
2018-09-06 15:30:25 +0300

    Add Stack

---
+++ src/main/java/lafoet/ch4/StackChar.java
@@ -0,0 +1,35 @@
+package lafoet.ch4;
+
+public class StackChar {
+    private int maxSize;
+    private char[] stackArray;
+    private int top;
+    //--------------------------------------------------------------
+    public StackChar(int max)    // constructor
+    {
+        maxSize = max;
+        stackArray = new char[maxSize];
+        top = -1;
+    }
+
+    //--------------------------------------------------------------
+    public void push(char j)  // put item on top of stack
+    {
+        stackArray[++top] = j;
+    }
+    //--------------------------------------------------------------
+    public char pop()         // take item from top of stack
+    {
+        return stackArray[top--];
+    }
+    //--------------------------------------------------------------
+    public char peek()        // peek at top of stack
+    {
+        return stackArray[top];
+    }
+    //--------------------------------------------------------------
+    public boolean isEmpty()  // true if stack is empty
+    {
+        return (top == -1);
+    }
+}
