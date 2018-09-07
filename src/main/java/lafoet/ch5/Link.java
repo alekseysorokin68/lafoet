f2ce573075bb139aa1fd911d8cf3754db3739f2a
Aleksey Sorokin <asorokin@megar.ru>
2018-09-07 10:31:08 +0300

    Add LinkList

---
+++ src/main/java/lafoet/ch5/Link.java
@@ -0,0 +1,18 @@
+package lafoet.ch5;
+
+public class Link {
+    public int iData;              // data item (key)
+    public double dData;           // data item
+    public Link next;              // next link in list
+    // -------------------------------------------------------------
+    public Link(int id, double dd) // constructor
+    {
+        iData = id;
+        dData = dd;
+    }
+    // -------------------------------------------------------------
+    public void displayLink()      // display ourself
+    {
+        System.out.print("{" + iData + ", " + dData + "} ");
+    }
+}
